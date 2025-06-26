60b5463d86c3d238874b82aa231f0ab28a5eb5ec
Electrogodzz <aidenhernando2010@gmail.com>
2025-06-25 20:48:52 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,21 @@
-<script>
-  const follower = document.getElementById('cursor-follower');
-  let mouseX = 0;
-  let mouseY = 0;
-  let followerX = 0;
-  let followerY = 0;
+const follower = document.getElementById('cursor-follower');
+let mouseX = 0;
+let mouseY = 0;
+let followerX = 0;
+let followerY = 0;
 
-  document.addEventListener('mousemove', (e) => {
-    mouseX = e.clientX;
-    mouseY = e.clientY;
-  });
+document.addEventListener('mousemove', (e) => {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+});
 
-  function animateFollower() {
-    // move 10% toward the cursor each frame
-    followerX += (mouseX - followerX - 20) * 0.1;
-    followerY += (mouseY - followerY - 20) * 0.1;
+function animateFollower() {
+  // Move 10% toward cursor each frame, offset by 20px so it lags a little
+  followerX += (mouseX - followerX - 20) * 0.1;
+  followerY += (mouseY - followerY - 20) * 0.1;
 
-    follower.style.transform = `translate(${followerX}px, ${followerY}px)`;
-    requestAnimationFrame(animateFollower);
-  }
+  follower.style.transform = `translate(${followerX}px, ${followerY}px)`;
+  requestAnimationFrame(animateFollower);
+}
 
-  animateFollower();
-</script>
-
+animateFollower();
